37d2f70018e4bd2ffd99bf3dfac8cdb85ccf58f3
Mathias de Mestral <mathiasdemestral@gmail.com>
2020-10-24 14:32:22 -0300

    Agregar migracion inicial

---
+++ src/AFORO255.MS.TEST.Invoice/Migrations/20201024171104_InitialCreate.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Invoices.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "invoices",
+                columns: table => new
+                {
+                    id_invoice = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    amount = table.Column<decimal>(nullable: false),
+                    state = table.Column<int>(nullable: false),
+                    customer_id = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_invoices", x => x.id_invoice);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "invoices");
+        }
+    }
+}
