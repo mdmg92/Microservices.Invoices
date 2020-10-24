2d6655c5b02ca887fbdf1e387a5ef0d81de849ba
Mathias de Mestral <mathiasdemestral@gmail.com>
2020-10-24 14:32:02 -0300

    Agregar Entity Framework Core

---
+++ src/AFORO255.MS.TEST.Invoice/Data/InvoiceDbContext.cs
@@ -0,0 +1,17 @@
+using Invoices.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Invoices.Data
+{
+    public class InvoiceDbContext : DbContext
+    {
+        public DbSet<Invoice> Invoices { get; set; }
+
+        public InvoiceDbContext(DbContextOptions<InvoiceDbContext> options) : base(options)
+        {
+            // this.Database.GetService<IRelationalDatabaseCreator>().Create();
+        }
+    }
+}
--- src/AFORO255.MS.TEST.Invoice/Startup.cs
+++ src/AFORO255.MS.TEST.Invoice/Startup.cs
@@ -1,5 +1,7 @@
+using Invoices.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -19,6 +21,11 @@ namespace Invoices
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<InvoiceDbContext>(options =>
+            {
+                options.UseNpgsql(Configuration.GetConnectionString("Postgres"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
