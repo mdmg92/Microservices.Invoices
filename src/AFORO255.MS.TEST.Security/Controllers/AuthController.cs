30cc40db40560dcecc7ff72fb866a46697dbfb14
Mathias de Mestral <mathiasdemestral@gmail.com>
2020-10-25 17:11:05 -0300

    Iniciar MS Security

---
+++ src/AFORO255.MS.TEST.Security/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Security.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
